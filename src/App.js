a4309eb2b5120addf4ff387b8324c1e6a7257637
Tuhin Khan <webwithjhankarvai@gmail.com>
2021-03-04 03:54:13 +0600

    another chart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Chart from './components/Chart/Chart.js' ;
+
 function App() {
 
   return (
   
-    <Chart></Chart>
+   <div>
+      <Chart></Chart>
+   </div>
+    
   );
 }
 
--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
 
 const data = [
     {name: 'Page A', uv: 4000, pv: 2400,  amt: 2400, },
@@ -12,32 +13,84 @@ const data = [
   ];
 const Chart = () => {
     return (
-        <div>
-       <LineChart
+      <div>
+      <AreaChart
       width={500}
-      height={300}
+      height={400}
       data={data}
       margin={{
-        top: 5,
+        top: 10,
         right: 30,
-        left: 20,
-        bottom: 5
+        left: 0,
+        bottom: 0
       }}
     >
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis dataKey="name" />
       <YAxis />
       <Tooltip />
-      <Legend />
-      <Line
+      <Area
+        type="monotone"
+        dataKey="uv"
+        stackId="1"
+        stroke="#8884d8"
+        fill="#8884d8"
+      />
+      <Area
         type="monotone"
         dataKey="pv"
-        stroke="#8884d8"
-        activeDot={{ r: 8 }}
+        stackId="1"
+        stroke="#82ca9d"
+        fill="#82ca9d"
       />
-      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-    </LineChart>
-        </div>
+      <Area
+        type="monotone"
+        dataKey="amt"
+        stackId="1"
+        stroke="#ffc658"
+        fill="#ffc658"
+      />
+    </AreaChart>
+
+
+    <AreaChart
+        width={500}
+        height={200}
+        data={data}
+        syncId="anyId"
+        margin={{
+          top: 10,
+          right: 30,
+          left: 0,
+          bottom: 0
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+      </AreaChart>
+      <p>Maybe some other content</p>
+      <AreaChart
+        width={500}
+        height={200}
+        data={data}
+        syncId="anyId"
+        margin={{
+          top: 10,
+          right: 30,
+          left: 0,
+          bottom: 0
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Area type="monotone" dataKey="pv" stroke="#82ca9d" fill="#82ca9d" />
+      </AreaChart>
+  </div>
     );
 };
 
